8f3a46547714ff9cf6bfde3b7661bdf9899504b5
HaCaoVi <icaovy2001@gmail.com>
2025-12-30 17:25:38 +0700

    UPDATE:: remove repository dont useuse

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -95,16 +95,5 @@ namespace e_commerce_basic.Repositories
         {
             return await _context.Products.FirstOrDefaultAsync(p => p.Id == id && !p.IsDeleted);
         }
-
-        public async Task<bool> UpdateAsync(Product product, UpdateProductDto updateProductDto)
-        {
-
-            return true;
-        }
-
-        public Task<bool> UpdateAsync(int id, Product product, UpdateProductDto updateProductDto)
-        {
-            throw new NotImplementedException();
-        }
     }
 }
